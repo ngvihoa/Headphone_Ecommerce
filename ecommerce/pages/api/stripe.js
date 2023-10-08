71a1b1708313543103872468828ad1f96709a2c4
NVH <93785793+ngvihoa@users.noreply.github.com>
2023-10-08 22:47:12 +0700

    fix the missing package 2

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
         line_items: req.body.map(item=>{
             const img = item.image[0].asset._ref;
             const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vjj82htn/production/').replace('-webp', '.webp');
-            console.log('IMAGE', newImage);
+            // console.log('IMAGE', newImage);
 
             return {
                 price_data: {
